5de80f9c9edb45e917c90be86cbeb9122ebacf20
juliusmuthui015-sketch <juliusmuthui015@gmail.com>
2026-03-12 11:56:34 +0300

    Implement train-test split in model training
    Added train-test split for model training.

--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
 
@@ -8,11 +9,12 @@ data = {
     "salary":[30000,40000,50000,60000,70000,80000]
 }
 
-df = pd.DataFrame(data)
+data = pd.DataFrame(data)
 
-X = df[["experience"]]
-y = df["salary"]
-
+X = data[["experience"]]
+y = data["salary"]
+#train model
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 model = LinearRegression()
 model.fit(X,y)
 
